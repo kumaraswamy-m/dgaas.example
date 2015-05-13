24d3923e07308f4209c46410df1e8d053a205d73
dgaas <dragos.cojocari@ro.ibm.com>
2015-05-13 16:31:05 +0300

    Initial commit of the Web App.

---
+++ src/com/ibm/dgaax/servlet/CORSFilter.java
@@ -0,0 +1,56 @@
+package com.ibm.dgaax.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class CORSFilter implements Filter
+{
+	private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods"; //$NON-NLS-1$
+	private static final String ALL_METHODS = "GET,POST,DELETE,PUT,OPTIOS,HEAD"; //$NON-NLS-1$
+	private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin"; //$NON-NLS-1$
+	private static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials"; //$NON-NLS-1$
+	private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers"; //$NON-NLS-1$
+	private static final String ALL_DOMAINS = "*"; //$NON-NLS-1$
+	
+	private static final String CORS_DOMAINS = "CORS_DOMAINS"; //$NON-NLS-1$
+	
+	@Override
+	public void destroy()
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
+	{
+		if ( res instanceof HttpServletResponse)
+		{
+			String domains = System.getenv( CORS_DOMAINS);
+			if ( domains == null || domains.isEmpty())
+			{
+				domains = ALL_DOMAINS;
+			}
+			
+			HttpServletResponse response = (HttpServletResponse) res;
+			response.setHeader( ACCESS_CONTROL_ALLOW_ORIGIN, domains);
+			response.setHeader( ACCESS_CONTROL_ALLOW_METHODS, ALL_METHODS);			
+			response.setHeader( ACCESS_CONTROL_ALLOW_CREDENTIALS, "true"); //$NON-NLS-1$
+			response.setHeader( ACCESS_CONTROL_ALLOW_HEADERS, "Origin,X-Requested-With,Content-Type,Accept,secret"); //$NON-NLS-1$
+		}
+		
+		chain.doFilter( req, res);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException
+	{
+		// TODO Auto-generated method stub
+	}
+}
---
+++ src/com/ibm/dgaax/servlet/CtoFService.java
@@ -0,0 +1,30 @@
+package com.ibm.dgaax.servlet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
+@Path("/ctof")
+@Api(value = "/ctof", description = "Celsius to Fahrenheit")
+public class CtoFService {
+	
+	@GET
+	@Produces("application/xml")
+	@ApiOperation(value = "Convert Celsius to Fahrenheit", notes = "More notes about this method", response = String.class)
+	@ApiResponses(value = {
+	  @ApiResponse(code = 400, message = "Invalid value") 
+	})
+	public String convertCtoF( @ApiParam(value = "Value in Celsius", required = true) @QueryParam(value="value")  double celsius) {
+
+		double fahrenheit = ((celsius * 9) / 5) + 32;
+		
+		return "<value>" + fahrenheit + "</value>";
+	}
+}
