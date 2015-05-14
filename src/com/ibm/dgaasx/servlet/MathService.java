b5c053f74805b51ab8e9915c2eb894c9e8e23908
dgaas <dragos.cojocari@ro.ibm.com>
2015-05-14 11:32:31 +0300

    math service

---
+++ src/com/ibm/dgaasx/servlet/MathService.java
@@ -0,0 +1,27 @@
+package com.ibm.dgaasx.servlet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+
+@Path("/ctof")
+@Api(value = "/ctof", description = "Celsius to Fahrenheit")
+public class MathService
+{
+
+	@GET
+	@Produces("text/plain")
+	@ApiOperation(value = "Convert Celsius to Fahrenheit", notes = "More notes about this method", response = String.class)
+	@ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid value") })
+	public String convertCtoF(@ApiParam(value = "Value in Celsius", required = true) @QueryParam(value = "value") double celsius)
+	{
+		return Double.toString(((celsius * 9) / 5) + 32);
+	}
+}
