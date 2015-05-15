64e758de2a2b578d1aded3b81a73c7881eb64375
dgaas <dragos.cojocari@ro.ibm.com>
2015-05-15 16:29:50 +0300

    Updated Swagger documentation

--- src/com/ibm/dgaasx/servlet/DGaaSXApplication.java
+++ src/com/ibm/dgaasx/servlet/DGaaSXApplication.java
@@ -18,7 +18,14 @@ public class DGaaSXApplication extends Application
 	{
 		BeanConfig beanConfig = new BeanConfig();
 		beanConfig.setContact("dragos.cojocari@ro.ibm.com");
-		beanConfig.setDescription("Document generation as a Service Example");
+		beanConfig.setDescription( "This API is an example of using DGaaS (Document Generation as a Service) and produces PDF documents from RSS 2.0 feeds." +
+								   "<br/><br/>The procedure is:" +
+								   "<ol><li>Invoke /dgasaax/api/rss2pdf with the RSS feed you want to convert to PDF.</li>"+
+								   "<li>monitor the process at /dgasaax/api/job/<jobid> returned by the first call.</li>"+
+								   "<li>access the PDF at /dgasaax/api/result/<resultid> if the job completes succesfully ( the status of the job becomes finished).</li>"+
+								   "</ol>" + 
+								   "<br/>For questions and support please see the Contact the Developer link."+
+								   "<br/><br/>The source code is hosted on Github at <a href='https://github.com/dgaas/dgaas.example'>https://github.com/dgaas/dgaas.example</a>.");
 		beanConfig.setVersion("1.0.0");
 		beanConfig.setBasePath(EnvironmentInfo.getBaseURL() + API);
 		beanConfig.setResourcePackage(CtoFService.class.getPackage().getName());
@@ -35,8 +42,8 @@ public class DGaaSXApplication extends Application
 		resources.add(com.wordnik.swagger.jersey.listing.ApiListingResourceJSON.class);
 		resources.add(com.wordnik.swagger.jersey.listing.JerseyResourceListingProvider.class);
 
-		resources.add(CtoFService.class);
-		resources.add(MathService.class);
+		//resources.add(CtoFService.class);
+		//resources.add(MathService.class);
 		resources.add(RSS2PDFService.class);
 		resources.add(JobProxyService.class);
 		resources.add(ResultProxyService.class);
