d47f918a8dbe4b9fd7923beaf86072038dda7edf
dgaas <dragos.cojocari@ro.ibm.com>
2015-05-15 12:40:13 +0300

    -

--- src/com/ibm/dgaasx/config/EnvironmentInfo.java
+++ src/com/ibm/dgaasx/config/EnvironmentInfo.java
@@ -5,14 +5,12 @@ public class EnvironmentInfo
 	public static final String getDGaaSURL()
 	{
 		String dgaasURL = System.getenv( "dgaas.url");
-		
 		return dgaasURL == null || dgaasURL.isEmpty() ? "http://giediprime.cluj.ro.ibm.com:9080/dgaas" : dgaasURL; 
 	}
 	
 	public static final String getBaseURL()
 	{
 		String baseURL = System.getenv( "base.url");
-		
-		return baseURL == null || baseURL.isEmpty() ? "http://dgaasx.mybluemix.net" : baseURL; 
+		return baseURL == null || baseURL.isEmpty() ? "http://dgaas-service.stage1.mybluemix.net" : baseURL; 
 	}
 }
