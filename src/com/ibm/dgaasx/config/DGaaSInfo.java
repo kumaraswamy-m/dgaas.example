bbee64a55445490ae8bc67599e9bb24e1cd6eea0
Dragos Cojocari <dragos.cojocari@ro.ibm.com>
2015-10-12 13:24:05 +0000

    Fix copyright character.

--- src/com/ibm/dgaasx/config/DGaaSInfo.java
+++ src/com/ibm/dgaasx/config/DGaaSInfo.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2015. All Rights Reserved.
+ * 
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp. 
+ *******************************************************************************/
 package com.ibm.dgaasx.config;
 
 public class DGaaSInfo
